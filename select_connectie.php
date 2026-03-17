980b55b96101bec6b3d996d23e489b2658967527
Sorarya <soraya.eljilali@student.rocva.nl>
2026-03-17 23:08:32 +0100

    port toe gevoegd

--- select_connectie.php
+++ select_connectie.php
@@ -1,11 +1,12 @@
 <?php
 // nieuwe connectie maken met database 
 $host="127.0.0.1:3306";
+$port = "3306";
 $user="root";
 $db="select_db";
 $pass="";
 try{
-    $conn=new pdo("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn=new pdo("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e){
     echo "Connection failed: " . $e->getMessage();
